6fe3b2f8bc12cae7f14ef156d68a2d92628736a4
surajnaik1595 <154022396+surajnaik1595@users.noreply.github.com>
2024-02-12 00:52:28 +0530

    Add files via upload

---
+++ Call function and print using double quotes and single quotes 2.py
@@ -0,0 +1,12 @@
+wish = "Hello World!"
+print(wish)
+hey = '"AskPython says" Hi'
+print(hey)
+famous = "'Taj Mahal' is in Agra."
+print(famous)
+
+#my test work
+aboutme = 'I am a good boy'
+print(aboutme)
+cat = 'Toasty'
+print(cat)
---
+++ Call function and print using double quotes and single quotes.py
@@ -0,0 +1,8 @@
+word = 'Ask?'
+print(word)
+sentence = 'Python Programming'
+print(sentence)
+name = '"Hi" ABC'
+print(name)
+congrat = "We congrat's you."
+print(congrat)
